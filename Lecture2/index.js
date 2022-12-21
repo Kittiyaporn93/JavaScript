4389c207b93bdf2b9ede2dfc6d6a8392923a20b5
Your Name <you@example.com>
2022-12-21 09:48:06 +0700

    update

---
+++ Lecture2/index.js
@@ -0,0 +1,11 @@
+const message = "Hello";
+const place = "World";
+
+//logging out a sting
+console.log('Hello,World!');
+
+//using subsitutions
+console.log('Hello,%s!', place);
+
+//using a string literal
+console.log(`${message}, ${place}!`);
