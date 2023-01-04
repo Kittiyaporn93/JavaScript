82d15f6ba33d091d9518f1ca03cc8fcbfefbe2d2
Your Name <you@example.com>
2023-01-04 11:49:44 +0700

    update

---
+++ Lecture3/test-2.js
@@ -0,0 +1,10 @@
+let values1 = ['Apple',1, false];
+let values2 = ['Fries', 2, true];
+let values3 = ['Mars', 9, 'Apple'];
+var common = [];
+for (var i = 0; i < values1.length; i++) {
+  if (values2.indexOf(values1[i]) > -1 && values3.indexOf(values1[i]) > -1) {
+    common.push(values1[i]);
+  }
+}
+console.log(common);
