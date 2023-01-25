e6e43436c0ab2748aeebde03332a68cc4049a454
Kittiyaporn Sawan <119995559+Kittiyaporn93@users.noreply.github.com>
2023-01-25 09:57:44 +0700

    update

---
+++ test/ts2.js
@@ -0,0 +1,15 @@
+function highestDigit(num) {
+    var highest = 0;
+    var numString = num.toString();
+    for (var i = 0; i < numString.length; i++) {
+      if (numString[i] > highest) {
+        highest = numString[i];
+      } 
+    }
+    return highest;
+  }
+
+  console.log(highestDigit(379));
+  console.log(highestDigit(2));
+  console.log(highestDigit(377401));
+ 
